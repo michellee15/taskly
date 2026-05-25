1dae00aae58aa9d3b5203f9bba46721150b495fc
michellee15 <e1459250@u.nus.edu>
2026-05-25 16:02:52 +0800

    Add task priority and update UI display

--- frontend/src/components/taskForm.jsx
+++ frontend/src/components/taskForm.jsx
@@ -1,5 +1,5 @@
 // handles input box + add button of adding task
-function TaskForm({title, setTitle, dueDate, setDueDate, handleAddTask}) {
+function TaskForm({title, setTitle, dueDate, setDueDate, priority, setPriority, handleAddTask}) {
   return (
     <form onSubmit={handleAddTask}>
       <input
@@ -13,6 +13,15 @@ function TaskForm({title, setTitle, dueDate, setDueDate, handleAddTask}) {
         value={dueDate}
         onChange={(e) => setDueDate(e.target.value)}
       />
+      <label>Priority</label>
+      <select
+        value={priority}
+        onChange={(e) => setPriority(e.target.value)}
+      >
+        <option value="low">Low</option>
+        <option value="medium">Medium</option>
+        <option value="high">High</option>
+      </select>
 
       <button type="submit">Add</button>
     </form>
